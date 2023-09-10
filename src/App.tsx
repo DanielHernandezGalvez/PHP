836f3aba5e0ff893b8731880bf4537b5be52bbc7
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-09-10 09:32:25 -0600

    main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [options, setOptions] = useState<string[]>([]);
+  const [randomOption, setRandomOption] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Cargar opciones desde el almacenamiento local al montar el componente
+    const storedOptions = localStorage.getItem('options');
+    if (storedOptions) {
+      setOptions(JSON.parse(storedOptions));
+    }
+  }, []);
+
+  useEffect(() => {
+    // Guardar opciones en el almacenamiento local cuando cambian
+    localStorage.setItem('options', JSON.stringify(options));
+  }, [options]);
+
+  const handleAddOption = () => {
+    if (inputValue.trim() !== '') {
+      setOptions([...options, inputValue.trim()]);
+      setInputValue('');
+    }
+  };
+
+  const handlePickRandomOption = () => {
+    if (options.length > 0) {
+      const randomIndex = Math.floor(Math.random() * options.length);
+      setRandomOption(options[randomIndex]);
+    } else {
+      setRandomOption(null);
+      alert('Agrega al menos una opción primero.');
+    }
+  };
 
   return (
-    <>
+    <div>
+      <h1>Selecciona una opción aleatoria</h1>
+      <input
+        type="text"
+        placeholder="Agregar opción"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={handleAddOption}>Agregar</button>
+      <button onClick={handlePickRandomOption}>Seleccionar aleatoriamente</button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Opciones:</h2>
+        <ul>
+          {options.map((option, index) => (
+            <li key={index}>{option}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {randomOption && (
+        <div>
+          <h2>Opción seleccionada aleatoriamente:</h2>
+          <p>{randomOption}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
