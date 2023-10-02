810b0913cecc37f48922ec3b08135ff903e1ba6d
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2023-10-02 14:02:06 -0600

    axios

--- 01-Node/index.js
+++ 01-Node/index.js
@@ -4,7 +4,20 @@
 // printName("Sonia")
 // console.log(sumatory(3, 4))
 
-const fs = require("fs")
+// const fs = require("fs");
+// const axios = require("axios");
+// const text = fs.readFileSync("./text.txt", "utf-8")
+// console.log(text)
+// console.log("file has been read")
+// const { get } = require("http");
+// axios
+//   .get("https://rickandmortyapi.com/api/character")
+//   .then((res) => console.log(res.data));
 
-const text = fs.readFileSync("./text.txt", "utf-8")
-console.log(text)
+// console.log("finished");
+
+setTimeout(() => {
+    console.log("this been late")
+}, 1500);
+
+console.log("first")
