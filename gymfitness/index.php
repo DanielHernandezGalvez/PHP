eaa9b3b9f4074cb9391cdaa2966af19a63a2b88a
unknown <hernandezgalvezalejandro@gmail.com>
2023-10-17 22:14:37 -0600

    dinamyc img

--- gymfitness/index.php
+++ gymfitness/index.php
@@ -1,9 +1,37 @@
-<?php
-    while( have_posts() ):  the_post();
+<!DOCTYPE html>
+<html lang="es">
 
-    the_title();
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gymfitness</title>
+</head>
 
-    the_content();
-    
-    endwhile;
-?>
+<body>
+    <header class="header">
+        <div class="contenedor barra-navegacion">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="">
+            </div>
+            <nav>
+                <a href="/nosotros">Nosotros</a>
+                <a href="/contacto">Contacto</a>
+                <a href="/blog">Blog</a>
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        <?php
+        while (have_posts()) :  the_post();
+
+            the_title();
+
+            the_content();
+
+        endwhile;
+        ?>
+    </main>
+</body>
+
+</html>
