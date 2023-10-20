aea53e2d9db31ec1e6fe195c719fab37430899c5
unknown <hernandezgalvezalejandro@gmail.com>
2023-10-20 16:08:59 -0600

    stylesheet

--- gymfitness/functions.php
+++ gymfitness/functions.php
@@ -8,3 +8,10 @@
     }
 
     add_action("init","gymfitness_menus");
+
+    // aqui se pone el stylesheet y en array el orden en el que las quiero
+    function gymfitness_scripts_styles() {
+        wp_enqueue_style("style", get_stylesheet_uri(), array(),"1.0.0");
+    }
+
+    add_action("wp_enqueue_scripts","gymfitness_scripts_styles");
--- gymfitness/index.php
+++ gymfitness/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gymfitness</title>
+    <?php wp_head() ?>
 </head>
 
 <body>
@@ -18,7 +19,7 @@
             $args = array(
                 // theme location es para mostrar menus en diferenter lugares
                 "theme_location" => "menu-principal",
-                "container"=> "nav",,
+                "container"=> "nav",
                 "container_class"=> "menu-principal nav",
             );
             wp_nav_menu($args);
