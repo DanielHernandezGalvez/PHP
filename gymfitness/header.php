d6939862f842ca950eb084d5a8e536e04e78bac3
unknown <hernandezgalvezalejandro@gmail.com>
2023-10-20 17:34:08 -0600

    page.php

---
+++ gymfitness/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gymfitness</title>
+    <?php wp_head() ?>
+</head>
+
+<body>
+    <header class="header">
+        <div class="contenedor barra-navegacion">
+            <div class="logo">
+                <!-- imagenes dinámicas -->
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="">
+            </div>
+            <?php
+            $args = array(
+                // theme location es para mostrar menus en diferenter lugares
+                "theme_location" => "menu-principal",
+                "container"=> "nav",
+                "container_class"=> "menu-principal nav",
+            );
+            wp_nav_menu($args);
+            ?>
+        </div>
+    </header>
--- gymfitness/index.php
+++ gymfitness/index.php
@@ -1,43 +1,18 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+    get_header();
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gymfitness</title>
-    <?php wp_head() ?>
-</head>
+<main>
+    <?php
+    while (have_posts()) :  the_post();
 
-<body>
-    <header class="header">
-        <div class="contenedor barra-navegacion">
-            <div class="logo">
-                <!-- imagenes dinámicas -->
-                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="">
-            </div>
-            <?php
-            $args = array(
-                // theme location es para mostrar menus en diferenter lugares
-                "theme_location" => "menu-principal",
-                "container"=> "nav",
-                "container_class"=> "menu-principal nav",
-            );
-            wp_nav_menu($args);
-            ?>
-        </div>
-    </header>
+        the_title();
 
-    <main>
-        <?php
-        while (have_posts()) :  the_post();
+        the_content();
 
-            the_title();
-
-            the_content();
-
-        endwhile;
-        ?>
-    </main>
+    endwhile;
+    ?>
+</main>
 </body>
 
 </html>
