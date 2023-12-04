ebb01049fc7638b851dee5a505bd666058155784
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-04 09:43:59 -0600

    arreglos indexados

--- curso/variables.php
+++ curso/variables.php
@@ -3,11 +3,23 @@ $fibonacci = 0.332342343;
 
 # conocer tipo de dato 
 
-echo gettype($fibonacci);
+// echo gettype($fibonacci);
 
 # constantes
 define("PI", 3.1416);
-echo PI;
+// echo PI;
 
 $semana = array("lunes", "martes", "miercoles", "jueves", "viernes");
 $semana[10] = "Dinz kramer";
+
+# Array asociativo 
+
+$kramer = array("tel" => 3319923923, "edad" => 26, "apellido" =>" Mr Crowley", "País" => "México");
+
+echo "<br />";
+echo "<br />";
+$kramer["tel"] = 2524;
+echo $kramer["tel"] . "<br />";
+echo $kramer["edad"] . "<br />";
+echo $kramer["apellido"] . "<br />";
+echo $kramer["País"] . "<br />";
