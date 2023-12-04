eeec4a822bf33c5e42b0d7d12089add665e0ddd2
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-04 17:27:17 -0600

    vardump

---
+++ curso/vardump.php
@@ -0,0 +1,16 @@
+<?php 
+
+$texto = "carlos";
+$numero = 2;
+$numero2 = 13;
+$arreglo = array("carlos", 4, 5);
+$arreglo_asociativo = array("dato" => "color rojo", "id" => 45);
+$booleano = false;
+
+echo "<pre>";
+var_dump($arreglo_asociativo);
+echo "</pre>";
+
+print_r($arreglo_asociativo);
+
+?>
