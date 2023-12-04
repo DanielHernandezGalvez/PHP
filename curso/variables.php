7830dd1aea2010ba6fa86debaa1a908cad3db319
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-04 10:05:26 -0600

    foreach

--- curso/variables.php
+++ curso/variables.php
@@ -34,9 +34,58 @@ echo $kramer["País"] . "<br />";
 $amigos = array(
     array("nombre" => "dinz", 20, array("sangre" => "o+")),
     array("alexis", 26),
-    array("Tocayo", 50)
+    array("Tocayo", 50),
+    array("Sonia", 24)
 );
 
 echo $amigos[0][1]["sangre"] . "<br />";
 echo $amigos[0]["nombre"] . "<br />";
-echo $amigos[2][0];
+echo $amigos[2][0] . "<br />";
+
+echo count($amigos);
+
+$ultimo = count($amigos) - 1;
+echo "<br />";
+$amigos[$ultimo];
+
+echo "<br />";
+
+$meses = array(
+    "enero",
+    'febrero',
+    'marzo',
+    'abril',
+    'mayo',
+    'junio',
+    'julio',
+    'agosto',
+    'septiembre',
+    'octubre',
+    'noviembre',
+    'diciembre'
+)
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meses del año</title>
+</head>
+
+<body>
+
+    <h1>Meses del año</h1>
+    <ul>
+        <?php
+        foreach ($meses as $mes) {
+            echo "<li>" . $mes . "</li>";
+        }
+        ?>
+    </ul>
+</body>
+
+</html>
