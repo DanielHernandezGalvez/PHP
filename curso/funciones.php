75158ffe15b2632482e2bbc44dd834504e345587
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-04 17:57:03 -0600

    area del triángulo

--- curso/funciones.php
+++ curso/funciones.php
@@ -15,4 +15,12 @@ echo "<br/>";
 $resultadi = sumar(3,56);
 echo $resultadi;
 
+# Área de un triángulo
+function calcular_area_triangulo($base, $altura) {
+    $resultado = ($base * $altura) / 2;
+    return $resultado;
+}
+
+echo calcular_area_triangulo(10, 10);
+
 ?>
