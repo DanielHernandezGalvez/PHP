2e943a194bb5d02dbdd794efb5764ce60134b7a4
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-04 13:38:57 -0600

    &&

--- curso/condicionales.php
+++ curso/condicionales.php
@@ -1,9 +1,14 @@
 <?php
-
+/*
+== igualdad
+!= diferencia cv 
+*/
 $autenticado = "puede pasar";
 
-if($autenticado == "puede pasar"){
+if($autenticado == "puede pasar" ){
     echo "<h1>puedes pasar</h1>";
+} else {
+    echo "<h1> no puedes pasar</h1>";
 };
 
 ?>
