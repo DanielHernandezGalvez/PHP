7bae9a5075f69ed7705e131313fbd7423c130265
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-04 16:44:09 -0600

    ciclo foreach

--- curso/ciclos.php
+++ curso/ciclos.php
@@ -34,14 +34,91 @@ $meses = array(
     "Diciembre"
 );
 
-for($i = 0; $i < count($meses); $i++){
+for ($i = 0; $i < count($meses); $i++) {
     echo $meses[$i] . "<br/>";
 }
 
-
+# while permite iterar con variables externas
 echo "<br/>";
 $counter = 0;
-while($counter < count($meses)){
+while ($counter < count($meses)) {
     echo $meses[$counter] . "<br />";
     $counter++;
 }
+
+# do while siempre se ejecuta aunque sea una vez
+echo "<br/> Do While <br/>";
+
+$i = 1;
+do {
+    echo $i . "<br />";
+    $i++;
+} while ($i <= 10);
+
+$alejandro = array(
+    "tel" => 331023392,
+    "edad" => 34,
+    "pais" => "mexico"
+);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Meses</h1>
+    <ul>
+        <?php
+        foreach ($meses as $mes) {
+            echo "<li>" . $mes . "</li>";
+        }
+        echo "<br/>";
+        foreach ($alejandro as $dato => $valor) {
+            echo "<li>" . $dato . ": " . $valor . "</li>";
+        }
+        ?>
+    </ul>
+</body>
+
+</html>
+
+<?php
+
+$paises = array("mexico", "argentina", "colombia", 
+"ecuador", "españa", "paraguay", "peru");
+
+foreach($paises as $pais) {
+    echo $pais . "<br />";
+
+    if($pais == "colombia") {
+        break;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Paises Latinoamericanos</h2>
+    <?php 
+        foreach($paises as $pais){
+            if ($pais == "españa") {
+                continue;
+            }
+            echo $pais . "<br/>";
+        }
+
+    ?>
+</body>
+</html>
