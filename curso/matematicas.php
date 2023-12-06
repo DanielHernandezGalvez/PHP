9e158f605e1bb44baa2cecbf0759035d17f08aad
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-06 13:45:08 -0600

    redondear y pi

--- curso/matematicas.php
+++ curso/matematicas.php
@@ -4,4 +4,17 @@ $numero = 15.734;
 # redondea al más cercano
 echo round($numero);
 echo "<br />";
+
+# Numero aleatorio
 echo rand(1, 67);
+echo "<br />";
+
+#redondea hacía arriba
+echo ceil(12.1);
+echo "<br />";
+
+# pi
+echo M_PI;
+echo "<br />";
+
+
