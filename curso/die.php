50c736172e09bb76d168bed3702927875514eadd
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-06 14:04:30 -0600

    die()

---
+++ curso/die.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "soy un echo";
+
+# quita todo debajo de esta función
+die();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hola mundo</h1>
+</body>
+</html>
