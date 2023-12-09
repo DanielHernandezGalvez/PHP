61e397c3c7c551228e67d10354d0ed455f62217c
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-09 17:22:00 -0600

    validar datos del formulario

--- curso/formularios/formulario.php
+++ curso/formularios/formulario.php
@@ -1,10 +1,23 @@
+<?php
+if (!empty($nombre)) {
+    $nombre = trim($nombre); // elimina espacios
+    $nombre = htmlspecialchars($nombre); // el usuario no puede inyectar código
+    $nombre = stripslashes($nombre); // evita que haya / y \
+    // $nombre = filter_var($nombre, FILTER_SANITIZE_STRING); // elimina los caracteres que sn código
+    $nombre = filter_var($nombre, FILTER_SANITIZE_EMAIL); // elimina los caracteres que sn código
+    echo "tu nombre es: $nombre";
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <form action="valida_envio.php" method="get">
         <input type="text" name="nombre" placeholder="nombre" id="">
@@ -12,4 +25,5 @@
         <input type="submit" name="enviar" id="">
     </form>
 </body>
+
 </html>
--- curso/formularios/valida_envio.php
+++ curso/formularios/valida_envio.php
@@ -4,4 +4,11 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
 } else {
     echo "se enviaron por post";
 }
+
+if(isset($_POST['submit'])){
+    echo "Se han enviado los datos correctamente";
+    print_r($_POST['submit']);
+}
+
+
 ?>
