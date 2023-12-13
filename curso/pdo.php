aeb498414d563ab113eb259fa7ee0b25726c800a
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-13 14:18:22 -0600

    metodo query

--- curso/pdo.php
+++ curso/pdo.php
@@ -1,11 +1,13 @@
 <?php
 
+$id = $_GET["id"];
+
 try {
     $conexion = new PDO('mysql:host=localhost;dbname=prueba_datos', "root", "");
     // echo "Conexión OK";
 
     // $resultados = $conexion->query("SELECT * FROM usuarios");
-    $resultados = $conexion->query("SELECT * FROM usuarios WHERE id = 2");
+    $resultados = $conexion->query("SELECT * FROM usuarios WHERE id = $id");
 
     foreach ($resultados as $fila) {
         echo $fila["id"] . " - " . $fila["nombre"] . "<br />";
