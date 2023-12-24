1f40334bfd8f99c0afd4cf9e85a13eb2825a328a
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-23 21:32:19 -0600

    html y css

---
+++ curso/practicas/paginacion/view.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paginacón</title>
+    <link rel="stylesheet" href="./styles.css">
+</head>
+
+<body>
+    <div class="contenedor">
+        <h1>Artículos</h1>
+        <section class="articulos">
+            <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sit a iusto voluptates architecto aspernatur dolore error repellat, impedit quia quas facere suscipit incidunt fugit reiciendis repellendus. Eaque, deleniti deserunt!</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sit a iusto voluptates architecto aspernatur dolore error repellat, impedit quia quas facere suscipit incidunt fugit reiciendis repellendus. Eaque, deleniti deserunt!</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sit a iusto voluptates architecto aspernatur dolore error repellat, impedit quia quas facere suscipit incidunt fugit reiciendis repellendus. Eaque, deleniti deserunt!</li>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex sit a iusto voluptates architecto aspernatur dolore error repellat, impedit quia quas facere suscipit incidunt fugit reiciendis repellendus. Eaque, deleniti deserunt!</li>
+            </ul>
+        </section>
+        <section class="paginacion">
+            <ul>
+                <li class="disabled">&laquo;</li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li>&raquo;</li>
+            </ul>
+        </section>
+
+    </div>
+</body>
+
+</html>
