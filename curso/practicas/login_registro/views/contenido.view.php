28bc48e869f5497dc602b01b020147a0d117df93
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-25 13:43:59 -0600

    formulario de registro

---
+++ curso/practicas/login_registro/views/contenido.view.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contenido</title>
+    <link rel="stylesheet" href="css/styles.css
+    ">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+</head>
+
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Contenido del sitio</h1>
+        <a href="cerrar.php">Cerrar Sesión</a>
+        <hr class="border">
+        <div class="contenido">
+            <article>
+                <p>Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Qui optio explicabo quo
+                    atque? Enim nam quis commodi neque
+                    laudantium autem labore, repellat quam
+                    aperiam quibusdam quia ipsam voluptate
+                    in laboriosam.
+                </p>
+                <p>Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Qui optio explicabo quo
+                    atque? Enim nam quis commodi neque
+                    laudantium autem labore, repellat quam
+                    aperiam quibusdam quia ipsam voluptate
+                    in laboriosam.
+                </p>
+            </article>
+        </div>
+    </div>
+</body>
+
+</html>
