747e6c8594ac710bcd9822dbb1840c178d17e0ce
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-25 13:50:33 -0600

    pagina de registro

---
+++ curso/practicas/login_registro/views/registrate.view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrate</title>
+    <link rel="stylesheet" href="css/styles.css
+    ">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+</head>
+
+<body>
+    <div class="contenedor">
+        <h1 class="titulo">Registrate</h1>
+        <hr class="border">
+        
+    </div>
+</body>
+
+</html>
