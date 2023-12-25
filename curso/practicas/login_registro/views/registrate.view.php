bf73fcd17405b4c7aa530651958e779b3c5c1b89
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-25 14:10:02 -0600

    login

--- curso/practicas/login_registro/views/registrate.view.php
+++ curso/practicas/login_registro/views/registrate.view.php
@@ -7,7 +7,7 @@
     <title>Registrate</title>
     <link rel="stylesheet" href="css/styles.css
     ">
-    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRsA==" crossorigin="anonymous">
 
 </head>
 
@@ -15,7 +15,13 @@
     <div class="contenedor">
         <h1 class="titulo">Registrate</h1>
         <hr class="border">
-        
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="formulario" name="login">
+        <div class="form-group">
+        <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="usuario">
+
+        </div>
+    </form>
     </div>
 </body>
 
