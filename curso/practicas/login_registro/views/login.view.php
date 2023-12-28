40c20b78b62fdc0b57f1c725fe277daa88f678be
unknown <hernandezgalvezalejandro@gmail.com>
2023-12-28 16:15:11 -0600

    login al contenido

--- curso/practicas/login_registro/views/login.view.php
+++ curso/practicas/login_registro/views/login.view.php
@@ -19,13 +19,21 @@
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="formulario" name="login">
 
             <div class="form-group">
-                <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="USsuario">
+                <i class="icono izquierda fa fa-user"></i><input type="text" name="usuario" class="usuario" placeholder="Usuario">
             </div>
           
             <div class="form-group">
                 <i class="icono izquierda fa fa-lock"></i><input type="password" name="password" class="password_btn" placeholder="Contraseña">
                 <i class="submit-btn fa fa-arrow-right" onclick="login.submit()"></i>
             </div>
+
+            <?php if (!empty($errores)) : ?>
+                <div class="error">
+                    <ul>
+                        <?php echo $errores ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
 
         </form>
         <p class="texto-registrate">¿Ya tienes cuenta?
