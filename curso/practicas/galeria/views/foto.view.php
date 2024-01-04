5ea183c3487e6c6617b69abe9a295889e8920674
unknown <hernandezgalvezalejandro@gmail.com>
2024-01-03 20:55:47 -0600

    mandar imagenes a la base de datos

---
+++ curso/practicas/galeria/views/foto.view.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Galería dinámica</h1>
+        </div>
+    </header>
+
+    <div class="contenedor">
+        <div class="foto">
+            <img src="fotos/1.jpg" alt="">
+            <p class="texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et voluptate necessitatibus rerum magni harum assumenda nam cum amet ipsam asperiores itaque voluptatibus sed quidem dolor labore, expedita rem tempora inventore.</p>
+            <a href="index.php">Regresar</a>
+        </div>
+    </div>
+
+    <footer>
+        <p class="copyright">Galería creada por el Capitán Kramer</p>
+    </footer>
+
+</body>
+
+</html>
---
+++ curso/practicas/galeria/views/index.view.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor">
+            <h1 class="titulo">Galería dinámica</h1>
+        </div>
+    </header>
+    <section class="fotos">
+        <div class="contenedor">
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/1.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/2.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/3.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/4.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/1.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/2.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/3.jpg" alt="">
+                </a>
+            </div>
+            <div class="thumb">
+                <a href="#">
+                    <img src="fotos/4.jpg" alt="">
+                </a>
+            </div>
+
+            <div class="paginacion">
+                <a href="#" class="izquierda"> Página Anterior </a>
+                <a href="#" class="derecha"> Página Siguiente </a>
+            </div>
+        </div>
+
+
+    </section>
+<footer>
+    <p class="copyright">Galería creada por el Capitán Kramer</p>
+</footer>
+
+</body>
+</html>
