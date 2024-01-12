dd2dceadea1eb59361b49124b9391fb17e45495d
unknown <hernandezgalvezalejandro@gmail.com>
2024-01-12 11:33:20 -0600

    paginacion el galería

--- curso/practicas/galeria/views/index.view.php
+++ curso/practicas/galeria/views/index.view.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/estilos.css">
     <title>Document</title>
 </head>
+
 <body>
     <header>
         <div class="contenedor">
@@ -14,58 +16,33 @@
     </header>
     <section class="fotos">
         <div class="contenedor">
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/1.jpg" alt="">
-                </a>
-            </div>
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/2.jpg" alt="">
-                </a>
-            </div>
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/3.jpg" alt="">
-                </a>
-            </div>
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/4.jpg" alt="">
-                </a>
-            </div>
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/1.jpg" alt="">
-                </a>
-            </div>
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/2.jpg" alt="">
-                </a>
-            </div>
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/3.jpg" alt="">
-                </a>
-            </div>
-            <div class="thumb">
-                <a href="#">
-                    <img src="fotos/4.jpg" alt="">
-                </a>
-            </div>
+
+            <?php foreach ($fotos as $foto) : ?>
+                <div class="thumb">
+                    <a href="foto.php?id=<?php echo $foto["id"] ?>">
+                        <img src="fotos/<?php echo $foto["imagen"] ?>" alt="<?php $foto["titulo"] ?>">
+                    </a>
+                </div>
+            <?php endforeach ?>
 
             <div class="paginacion">
-                <a href="#" class="izquierda"> Página Anterior </a>
-                <a href="#" class="derecha"> Página Siguiente </a>
+                <?php if ($pagina_actual > 1) : ?>
+                    <a href="index.php?p=<?php echo $pagina_actual - 1; ?>" class="izquierda"> Página Anterior </a>
+                <?php endif; ?>
+
+                <?php if ($total_paginas != $pagina_actual) : ?>
+                    <a href="index.php?p=<?php echo $pagina_actual + 1 ?>" class="derecha"> Página Siguiente </a>
+                <?php endif; ?>
+          
             </div>
         </div>
 
 
     </section>
-<footer>
-    <p class="copyright">Galería creada por el Capitán Kramer</p>
-</footer>
+    <footer>
+        <p class="copyright">Galería creada por el Capitán Kramer</p>
+    </footer>
 
 </body>
+
 </html>
