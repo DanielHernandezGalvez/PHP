720a8ed560f1032751b0b579130ea7d8303abef7
unknown <hernandezgalvezalejandro@gmail.com>
2024-02-28 17:45:23 -0600

    identation productos.php

--- udemy/PDO/productos.php
+++ udemy/PDO/productos.php
@@ -1,7 +1,8 @@
 <?php
-require_once("conectar.php");
-$d = new Datos();
-$datos = $d->getDatos(("select category.id, category.name, category.type, category.description as category from category
-inner join categoris on categorias.id = category.categorias_id order by category.id desc;
-"));
-print_r($datos);
+    require_once("conectar.php");
+    $d = new Datos();
+    $datos = $d->getDatos(("select category.id, category.name, category.type, category.description as category from category
+    inner join categoris on categorias.id = category.categorias_id order by category.id desc;
+    "));
+    print_r($datos);
+?>
