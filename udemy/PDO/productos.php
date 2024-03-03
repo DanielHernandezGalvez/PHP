250e64cf6fb74a0d965900546b340d029ee892eb
Daniel Alejandro Hernández Gálvez <109681970+DanielHernandezGalvez@users.noreply.github.com>
2024-03-03 10:33:15 -0600

    Update productos.php

--- udemy/PDO/productos.php
+++ udemy/PDO/productos.php
@@ -4,5 +4,6 @@
     $datos = $d->getDatos(("select category.id, category.name, category.type, category.description as category from category
     inner join categoris on categorias.id = category.categorias_id order by category.id desc;
     "));
+
     print_r($datos);
 ?>
